27d3f48bf11788cc8d501e7137394f3c1b96a20b
Faelixx <sasha.taylor7@gmail.com>
2023-11-09 18:22:50 -0700

    Added typewriter.js to repo

---
+++ typewriter.js
@@ -0,0 +1,22 @@
+const sentence = "hello there from lighthouse labs";
+
+// Our goal is to animate the sentance one character at a time.
+// Like someone is typing to you.
+
+const letterPrint = function(string) {
+  for (const letter in string) {
+    setTimeout(() => {
+      process.stdout.write(string[letter]);
+    }, letter * 100);
+  }
+};
+
+
+letterPrint(sentence);
+setTimeout(() => {
+  console.log('');
+}, 5000);
+
+
+
+// console.log(sentence);
